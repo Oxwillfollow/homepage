921395c45ae34bc85c40703e019385bdc5c52c01
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-05-14 18:12:54 +0300

    Add projects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+import profilePic from "./images/profile-pic.png";
+import githubIcon from "./images/github.png";
+import imgBattleship from "./images/screenshots/screenshot-battleship.jpg";
+import imgTodoList from "./images/screenshots/screenshot-todolist.jpg";
+
+const screenshots = {
+  imgBattleship,
+  imgTodoList,
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  let myProfileImg = document.querySelector(".about-me-img");
+  let myGithubIcons = document.querySelectorAll(".github-icon");
+  let myProjectScreenshots = document.querySelectorAll(".project-screenshot");
+
+  myProfileImg.src = profilePic;
+
+  myGithubIcons.forEach((icon) => {
+    icon.src = githubIcon;
+    icon.style.width = "32px";
+    icon.style.height = "32px";
+  });
+
+  myProjectScreenshots.forEach((screenshot) => {
+    screenshot.src = screenshots[screenshot.id];
+  });
+});
