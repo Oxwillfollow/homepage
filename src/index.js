9d5c8bef455ebe1d175c5a5a0118a5e170b7265c
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-05-14 23:20:01 +0300

    Add more projects, add contact placeholders

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import "./styles.css";
-import profilePic from "./images/profile-pic.png";
-import githubIcon from "./images/github.png";
+import imgProfilePic from "./images/profile-pic.png";
+import imgGithubIcon from "./images/github.png";
 import imgBattleship from "./images/screenshots/screenshot-battleship.jpg";
 import imgTodoList from "./images/screenshots/screenshot-todolist.jpg";
+import imgWeatherApp from "./images/screenshots/screenshot-weatherapp.png";
+import imgCalculator from "./images/screenshots/screenshot-calculator.png";
+import imgLibraryApp from "./images/screenshots/screenshot-library.png";
+import imgSignupForm from "./images/screenshots/screenshot-signup.png";
+import imgAdminDashboard from "./images/screenshots/screenshot-dashboard.png";
+import imgRestaurantPage from "./images/screenshots/screenshot-restaurant.png";
 
 const screenshots = {
   imgBattleship,
   imgTodoList,
+  imgWeatherApp,
+  imgCalculator,
+  imgLibraryApp,
+  imgSignupForm,
+  imgAdminDashboard,
+  imgRestaurantPage,
 };
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,10 +26,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let myGithubIcons = document.querySelectorAll(".github-icon");
   let myProjectScreenshots = document.querySelectorAll(".project-screenshot");
 
-  myProfileImg.src = profilePic;
+  myProfileImg.src = imgProfilePic;
 
   myGithubIcons.forEach((icon) => {
-    icon.src = githubIcon;
+    icon.src = imgGithubIcon;
     icon.style.width = "32px";
     icon.style.height = "32px";
   });
